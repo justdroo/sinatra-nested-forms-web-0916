8178fa46213555e2c46c4db312d26e2d962a4b48
Flatiron School <flatironschool@Drew-2.local>
2016-10-15 11:55:05 -0400

    Completed!

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  @@all = []
+  attr_accessor :name, :type, :booty
+
+  def initialize(params)
+    @name = params[:pirate][:ships][Ship.all.count][:name]
+    @type = params[:pirate][:ships][Ship.all.count][:type]
+    @booty = params[:pirate][:ships][Ship.all.count][:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
